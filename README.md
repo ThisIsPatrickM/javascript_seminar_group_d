cb9f59acecd4df8ebf2291ed7a93e3786b114bac
Michaela <m.finger@mail.de>
2020-09-21 00:16:22 +0200

    init express.js project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # javascript_seminar_group_d
+
+requires latest node.js 
+
+install project with 'npm install'
+
+run development mode with 'npm run dev'
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const config = require('./config');
+const app = express();
+
+
+// test request
+app.get('api/test_template', (req, res) => {
+    const template_test = require('./templates/test.json');
+    res.json(template_test);
+});
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(config.PORT, () => console.log(`Server is running on ${config.PORT}`));
