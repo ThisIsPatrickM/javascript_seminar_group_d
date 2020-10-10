e2d578ddd03e0fe7005e35c9928ea50e2f4153a3
Patrick <patrick.molz@web.de>
2020-10-10 10:38:29 +0200

    Different CORS Setting

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const games = require('./gameLogic.js');
 
 const INDEX = "./public/index.html";
 const PORT = 55555;
-const cors = require('cors');
 const ioApp = express();
+const cors = require('cors');
 ioApp.use(cors());
+ioApp.options('*', cors());
 const ioServer = ioApp.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
